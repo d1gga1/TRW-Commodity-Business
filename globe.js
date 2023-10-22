b230b9790f727359dc5f6ab3a50884480588826e
Farhod <ubaydullaevfarhod18@gmail.com>
2023-10-22 14:07:31 +0500

    add: build tool for building our project

--- globe.js
+++ globe.js
@@ -1,85 +1,84 @@
 /**
  * ---------------------------------------
  * This demo was created using amCharts 4.
- * 
+ *
  * For more information visit:
  * https://www.amcharts.com/
- * 
+ *
  * Documentation is available at:
  * https://www.amcharts.com/docs/v4/
  * ---------------------------------------
  */
 
 // Themes begin
-am4core.useTheme(am4themes_animated);
+am4core.useTheme(am4themes_animated)
 // Themes end
 
-var chart = am4core.create("chartdiv", am4maps.MapChart);
+var chart = am4core.create('chartdiv', am4maps.MapChart)
 
 // Set map definition
-chart.geodata = am4geodata_worldLow;
+chart.geodata = am4geodata_worldLow
 
 // Set projection
-chart.projection = new am4maps.projections.Orthographic();
-chart.panBehavior = "rotateLongLat";
-chart.deltaLatitude = -20;
-chart.padding(20,20,20,20);
+chart.projection = new am4maps.projections.Orthographic()
+chart.panBehavior = 'rotateLongLat'
+chart.deltaLatitude = -20
+chart.padding(20, 20, 20, 20)
 
 // Create map polygon series
-var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries())
 
 // Make map load polygon (like country names) data from GeoJSON
-polygonSeries.useGeodata = true;
+polygonSeries.useGeodata = true
 //polygonSeries.include = ["BR", "UA", "MX", "CI"];
 
 // Configure series
-var polygonTemplate = polygonSeries.mapPolygons.template;
-polygonTemplate.tooltipText = "{name}";
-polygonTemplate.fill = am4core.color("white");
-polygonTemplate.stroke = am4core.color("#000033");
-polygonTemplate.strokeWidth = 0.5;
-polygonTemplate.cursorOverStyle = am4core.MouseCursorStyle.pointer;
-polygonSeries.mapPolygons.template.events.on("hit", function (event) {
-    var countryId = event.target.dataItem.dataContext.id;
-    var url;
+var polygonTemplate = polygonSeries.mapPolygons.template
+polygonTemplate.tooltipText = '{name}'
+polygonTemplate.fill = am4core.color('white')
+polygonTemplate.stroke = am4core.color('#000033')
+polygonTemplate.strokeWidth = 0.5
+polygonTemplate.cursorOverStyle = am4core.MouseCursorStyle.pointer
+polygonSeries.mapPolygons.template.events.on('hit', function (event) {
+  var countryId = event.target.dataItem.dataContext.id
+  var url
 
-    if (countryId === "IT") {
-        url = "https://www.youtube.com/results?search_query=buyer+or+seller+choose+page+html"; // Sostituisci con l'URL desiderato per l'Italia
-    } else {
-        url = countryURLs[countryId];
-    }
+  if (countryId === 'IT') {
+    url =
+      'https://www.youtube.com/results?search_query=buyer+or+seller+choose+page+html' // Sostituisci con l'URL desiderato per l'Italia
+  } else {
+    url = countryURLs[countryId]
+  }
 
-    if (url) {
-        window.location.href = url;
-    }
-});
-
-var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
-graticuleSeries.mapLines.template.line.stroke = am4core.color("#ffffff");
-graticuleSeries.mapLines.template.line.strokeOpacity = 0.08;
-graticuleSeries.fitExtent = false;
-
-
-chart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 0.1;
-chart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#ffffff");
-
-// Create hover state and set alternative fill color
-var hs = polygonTemplate.states.create("hover");
-hs.properties.fill = chart.colors.getIndex(0).brighten(-0.5);
-
-let animation;
-setTimeout(function(){
-  animation = chart.animate({property:"deltaLongitude", to:0}, 2000);
-}, 3000);
-
-chart.seriesContainer.events.on("down", function(){
-//  animation.stop();
+  if (url) {
+    window.location.href = url
+  }
 })
 
-chart.seriesContainer.events.on("down", function(){
-    if (animation) {
-      animation.stop();
-    }
-  });
+var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries())
+graticuleSeries.mapLines.template.line.stroke = am4core.color('#ffffff')
+graticuleSeries.mapLines.template.line.strokeOpacity = 0.08
+graticuleSeries.fitExtent = false
 
+chart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 0.1
+chart.backgroundSeries.mapPolygons.template.polygon.fill =
+  am4core.color('#ffffff')
 
+// Create hover state and set alternative fill color
+var hs = polygonTemplate.states.create('hover')
+hs.properties.fill = chart.colors.getIndex(0).brighten(-0.5)
+
+let animation
+setTimeout(function () {
+  animation = chart.animate({ property: 'deltaLongitude', to: 0 }, 2000)
+}, 3000)
+
+chart.seriesContainer.events.on('down', function () {
+  //  animation.stop();
+})
+
+chart.seriesContainer.events.on('down', function () {
+  if (animation) {
+    animation.stop()
+  }
+})
